092cc3359f2b4406318ef583970469605fb4d8fd
Arielreza <arielreza111@gmail.com>
2023-09-29 10:48:42 +0700

    Ternary Operator changes.

--- PemilihanPercobaan118.java
+++ PemilihanPercobaan118.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class PemilihanPercobaan118 {
     public static void main(String[] args) {
         Scanner input00 = new Scanner(System.in);
-        System.out.println("Masukkan angka:  ");
+
+        System.out.println("Masukkan angka: ");
         int angka = input00.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
-    }
+        String status= (angka %2 == 0) ? "genap":"ganjil";
+        System.out.println("Angka "+angka+" adalah bilangan "+status);
+
+        }
 }
