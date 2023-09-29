7d213fdea80fb5cd136f78d80fdf0a9b3dd0de25
Arielreza <arielreza111@gmail.com>
2023-09-29 13:28:06 +0700

    Penambahan Nilai Huruf

--- PemilihanPercobaan218.java
+++ PemilihanPercobaan218.java
@@ -14,8 +14,24 @@ public class PemilihanPercobaan218 {
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
         String message = total < 65 ? "Remidi" : "Tidak remidi";
+        if (total >=80 && total <100){
+            message= "Nilai Huruf   : A";
+        }else if (total >=73 && total <80){
+            message= "Nilai Huruf   : B+";
+        
+        }else if (total >=65 && total <73){
+            message= "Nilai Huruf   : B";
+        }else if (total >=60 && total<65){
+            message= "Nilai Huruf   : C+";
+        }else if (total >=50 && total<60){
+            message= "Nilai Huruf   : C";
+        }else if (total >=39 && total<50){
+            message= "Nilai Huruf   : D";
+        }else if (total >=0 && total<39){
+            message= "Nilai Huruf   : E";
+        }
 
-        System.out.println("Nilai akhir = " + total + "sehingga " + message);
+        System.out.println("Nilai akhir = " + total + " sehingga "+ message);
 
     }
 }
