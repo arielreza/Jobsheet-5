e9748ee7515925df959db4b37d1dff031dd39329
Arielreza <arielreza111@gmail.com>
2023-09-29 13:36:07 +0700

    update input no absen

--- PemilihanPercobaan118.java
+++ PemilihanPercobaan118.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan118 {
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input18 = new Scanner(System.in);
 
         System.out.println("Masukkan angka: ");
-        int angka = input00.nextInt();
+        int angka = input18.nextInt();
 
         String status= (angka %2 == 0) ? "genap":"ganjil";
         System.out.println("Angka "+angka+" adalah bilangan "+status);
